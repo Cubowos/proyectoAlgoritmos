5b40387db3e21e01fb524e1fe3d2d681dc539cad
Cubowos <erodrigez3@gmail.com>
2021-12-17 22:59:01 -0600

    Primeras letras

--- descifrar1texto.py
+++ descifrar1texto.py
@@ -10,12 +10,14 @@ def cargarPalabras():
 alfabeto="abcdefghijklmnopqrstuvwxyz"
 dic = cargarPalabras()
 
+
 cifrado="Zdhrmxxj, bu hdcdllj rim dpmpd ymh zocxojhm dzorj bu cdeej, zd uju buj qbdxbuqbm. Qbmtej cdeej tm um dufdpd tmzyhm ou cohj rju dffjttj bu ydoj fo teopdxo, ymhroj Zdhrmxxj x�dpmpd riodzdej �Cdeej rju cxo Teopdxo O fbm mhduj rhmtrobeo dttomzm, m ox cdeej to mhd tmzyhm fozjtehdej zjxej nbhgj m nmxorm fo dobedhm Zdhrmxxj, qbdufj mhd ou fonnorjxed. Bu cojhuj ydhemroydhjuj dxxd nmted fmx ydmtm, fjpm Zdhrmxxj rjujggm Tdufhd, xd nocxod fmx horrj Tocujhjeej fmx yjtej. O fbm to ehjpdhjuj tbgoej tozydeoro, zd Tdufhd cod tdympd rim uju dphmggm yjebej nhmqbmuedhm Zdhrmxxj ymhrim tbj ydfhm uju x�dphmggm ymhzmttj. Oundeeo, ox ydfhm fo Tdufhd uju pjxmpd rim xd nocxod dpmttm dzoro ehd cxo dgoedueo fmx ydmtm, fd xbo hoemubeo fmo tmzyxorojeeo."
 cifrado= cifrado.lower()
-print(cifrado)
+#d=e m=a QUIZAS
 
-'''
-def traduccion(cifrado): #funcion de traduccion
-    nuevoTexto={'a':'m','b':'','c':'','d':'e','e':'','f':'','g':'','i':'','j':'o','k':'','l':'','m':'','n':'','o':'i','p':'','q':'','s':'','t':'','u':'','v':'','w':'','y':'','z':''}
-    print(nuevoTexto,"\nEste es el diccionario que usaremos para descifrarlo ")
-'''
+#probando con dpmpd
+for pal in dic:
+    if  (len(pal)==5):
+        if(pal[0]==pal[-1]) :
+            if(pal[1]==pal[3]):
+                print(pal)
